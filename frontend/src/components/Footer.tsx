e0809f2f0893fc6af18a4da124be435abfde577a
ksh0123 <shnae.k@gmail.com>
2024-08-19 23:25:03 +0900

    Add link to github logo

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -4,12 +4,10 @@ import { RiNotionFill } from "react-icons/ri";
 
 const Footer: FC = () => {
   return (
-    <div className="sticky bottom-2 flex flex-row justify-between items-center mx-20 py-2 z-20">
+    <div className="sticky bottom-2 flex flex-row justify-between items-center mx-10 md:mx-20 py-2 z-20">
       <div className="font-light text-xs whitespace-pre">{`Copyright © 2024 멋쟁이사자처럼 All rights reserved.\nDesigned by Shinhye Kang`}</div>
       <div className="flex flex-row gap-5">
-        <FaGithub className="scale-[1.5]" />
-        <RiNotionFill className="scale-[1.8]" />
-        <FaDiscord className="scale-[1.6]" />
+        <a target="_blank" href="https://github.com/ksh0123/NFT-Sale-Page"><FaGithub className="scale-[1.5]" /></a>
       </div>
     </div>
   );
